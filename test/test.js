87fdfa3d47a1de4fb5eea4b7d4379401b84874f3
yama-dev <tatsuya.yamamoto69@gmail.com>
2020-12-12 13:26:35 +0900

    adjust test

--- test/test.js
+++ test/test.js
@@ -8,73 +8,73 @@ let options = {};
 
 options.template = [
   {
-    data_type: "news",
+    data_type: 'news',
     list: {
-      base: "template/_index.html.ejs",
-      path: "src/news/[page]/",
-      name: "index.html"
+      base: 'template/_index.html.ejs',
+      path: 'src/news/[page]/',
+      name: 'index.html'
     },
     detail: {
-      base: "template/_index.html.ejs",
-      path: "src/news/[slug]/",
-      name: "index.html"
+      base: 'template/_index.html.ejs',
+      path: 'src/news/[slug]/',
+      name: 'index.html'
     }
   },
   {
-    data_type: "live",
+    data_type: 'live',
     list: {
-      base: "template/_index.html.ejs",
-      path: "src/live/[page]/",
-      name: "index.html"
+      base: 'template/_index.html.ejs',
+      path: 'src/live/[page]/',
+      name: 'index.html'
     },
     detail: {
-      base: "template/_index.html.ejs",
-      path: "src/live/[slug]/",
-      name: "index.html"
+      base: 'template/_index.html.ejs',
+      path: 'src/live/[slug]/',
+      name: 'index.html'
     }
   }
 ];
 
 options.data = {
   news: [
-    { data_type: "news", slug: "12", title: "title test", date: "2020-11-11 00:00:00", published: true, article: "<div>article text</div>", },
-    { data_type: "news", slug: "13", title: "title test", date: "2020-11-12 00:00:00", published: true, article: "<div>article text</div>", },
-    { data_type: "news", slug: "14", title: "title test", date: "2020-11-13 00:00:00", published: true, article: "<div>article text</div>", },
-    { data_type: "news", slug: "15", title: "title test", date: "2020-11-14 00:00:00", published: true, article: "<div>article text</div>", },
-    { data_type: "news", slug: "16", title: "title test", date: "2020-11-15 00:00:00", published: true, article: "<div>article text</div>", },
+    { data_type: 'news', slug: '12', title: 'title test', date: '2020-11-11 00:00:00', published: true, article: '<div>article text</div>', },
+    { data_type: 'news', slug: '13', title: 'title test', date: '2020-11-12 00:00:00', published: true, article: '<div>article text</div>', },
+    { data_type: 'news', slug: '14', title: 'title test', date: '2020-11-13 00:00:00', published: true, article: '<div>article text</div>', },
+    { data_type: 'news', slug: '15', title: 'title test', date: '2020-11-14 00:00:00', published: true, article: '<div>article text</div>', },
+    { data_type: 'news', slug: '16', title: 'title test', date: '2020-11-15 00:00:00', published: true, article: '<div>article text</div>', },
     {
-      data_type: "news",
-      slug: "yamamoto",
-      title: "title test",
-      date: "2020-11-10 00:00:00",
+      data_type: 'news',
+      slug: 'yamamoto',
+      title: 'title test',
+      date: '2020-11-10 00:00:00',
       published: true,
-      article: "<div>article text</div>",
+      article: '<div>article text</div>',
     },
     {
-      data_type: "news",
-      slug: "adachi",
-      title: "title test",
-      date: "2020-12-10 00:00:00",
+      data_type: 'news',
+      slug: 'adachi',
+      title: 'title test',
+      date: '2020-12-10 00:00:00',
       published: false,
-      article: "<div>article text</div>",
+      article: '<div>article text</div>',
     }
   ],
   live: [
     {
-      data_type: "live",
-      slug: "yamamoto",
-      title: "title test",
-      date: "2020-11-10 00:00:00",
+      data_type: 'live',
+      slug: 'yamamoto',
+      title: 'title test',
+      date: '2020-11-10 00:00:00',
       published: false,
-      article: "<div>article text</div>",
+      article: '<div>article text</div>',
     },
     {
-      data_type: "live",
-      slug: "adachi",
-      title: "title test",
-      date: "2020-12-10 00:00:00",
+      data_type: 'live',
+      slug: 'adachi',
+      title: 'title test',
+      date: '2020-12-10 00:00:00',
       published: true,
-      article: "<div>article text</div>",
+      article: '<div>article text</div>',
     }
   ]
 };
