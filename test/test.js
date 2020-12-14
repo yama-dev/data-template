cf4ca1b3d139f4458d68f1db3183e079c8fafa80
yama-dev <tatsuya.yamamoto69@gmail.com>
2020-12-14 16:53:10 +0900

    adjust test

--- test/test.js
+++ test/test.js
@@ -4,32 +4,40 @@ const fs = require('fs-extra');
 const { execSync } = require('child_process');
 const dataTemplate = require('../dataTemplate.js');
 
-let options = {};
+let options = {
+  logLevel: false,
+};
 
 options.template = [
   {
     data_type: 'news',
+    posts_per_page: 10,
     list: {
-      base: 'template/_index.html.ejs',
-      path: 'src/news/[page]/',
+      base: 'template/news/_index.html.ejs',
+      path: 'dist/news/',
+      name: 'index.html'
+    },
+    page: {
+      base: 'template/news/_index.html.ejs',
+      path: 'dist/news/[page]/',
       name: 'index.html'
     },
     detail: {
-      base: 'template/_index.html.ejs',
-      path: 'src/news/[slug]/',
+      base: 'template/news/_detail.html.ejs',
+      path: 'dist/news/[slug]/',
       name: 'index.html'
     }
   },
   {
     data_type: 'live',
     list: {
-      base: 'template/_index.html.ejs',
-      path: 'src/live/[page]/',
+      base: 'template/live/_index.html.ejs',
+      path: 'dist/live/',
       name: 'index.html'
     },
     detail: {
-      base: 'template/_index.html.ejs',
-      path: 'src/live/[slug]/',
+      base: 'template/live/_detail.html.ejs',
+      path: 'dist/live/[slug]/',
       name: 'index.html'
     }
   }
@@ -37,11 +45,19 @@ options.template = [
 
 options.data = {
   news: [
-    { data_type: 'news', slug: '12', title: 'title test', date: '2020-11-11 00:00:00', published: true, article: '<div>article text</div>', },
-    { data_type: 'news', slug: '13', title: 'title test', date: '2020-11-12 00:00:00', published: true, article: '<div>article text</div>', },
-    { data_type: 'news', slug: '14', title: 'title test', date: '2020-11-13 00:00:00', published: true, article: '<div>article text</div>', },
-    { data_type: 'news', slug: '15', title: 'title test', date: '2020-11-14 00:00:00', published: true, article: '<div>article text</div>', },
-    { data_type: 'news', slug: '16', title: 'title test', date: '2020-11-15 00:00:00', published: true, article: '<div>article text</div>', },
+    { data_type: 'news', slug: '10', title: 'title test', date: '2020-11-03 00:00:00', published: false, article: '<div>article text</div>', },
+    { data_type: 'news', slug: '11', title: 'title test', date: '2020-11-04 00:00:00', published: false, article: '<div>article text</div>', },
+    { data_type: 'news', slug: '12', title: 'title test', date: '2020-11-05 00:00:00', published: false, article: '<div>article text</div>', },
+    { data_type: 'news', slug: '13', title: 'title test', date: '2020-11-06 00:00:00', published: false, article: '<div>article text</div>', },
+    { data_type: 'news', slug: '14', title: 'title test', date: '2020-11-07 00:00:00', published: false, article: '<div>article text</div>', },
+    { data_type: 'news', slug: '15', title: 'title test', date: '2020-11-10 00:00:00', published: false, article: '<div>article text</div>', },
+    { data_type: 'news', slug: '16', title: 'title test', date: '2020-11-11 00:00:00', published: false, article: '<div>article text</div>', },
+    { data_type: 'news', slug: '17', title: 'title test', date: '2020-11-12 00:00:00', published: false, article: '<div>article text</div>', },
+    { data_type: 'news', slug: '18', title: 'title test', date: '2020-11-13 00:00:00', published: false, article: '<div>article text</div>', },
+    { data_type: 'news', slug: '19', title: 'title test', date: '2020-11-14 00:00:00', published: false, article: '<div>article text</div>', },
+    { data_type: 'news', slug: '20', title: 'title test', date: '2020-11-15 00:00:00', published: false, article: '<div>article text</div>', },
+    { data_type: 'news', slug: '21', title: 'title test', date: '2020-11-16 00:00:00', published: false, article: '<div>article text</div>', },
+    { data_type: 'news', slug: '22', title: 'title test', date: '2020-11-17 00:00:00', published: false, article: '<div>article text</div>', },
     {
       data_type: 'news',
       slug: 'yamamoto',
@@ -62,14 +78,6 @@ options.data = {
   live: [
     {
       data_type: 'live',
-      slug: 'yamamoto',
-      title: 'title test',
-      date: '2020-11-10 00:00:00',
-      published: false,
-      article: '<div>article text</div>',
-    },
-    {
-      data_type: 'live',
       slug: 'adachi',
       title: 'title test',
       date: '2020-12-10 00:00:00',
@@ -79,15 +87,20 @@ options.data = {
   ]
 };
 
-let stdout_src = null;
-let stdout_template = null;
-let stdout_read = null;
+let stdout_src = '';
+let stdout_template = '';
+let stdout_read = '';
 
 describe('Run function', function() {
   it('create template file', function() {
     try {
-      fs.mkdirSync('template', { recursive: true });
-      fs.writeFileSync('template/_index.html.ejs', '<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8"><title></title></head><body><%=slug%> - <%=title%></body></html>');
+      fs.mkdirSync('template/news', { recursive: true });
+      fs.writeFileSync('template/news/_index.html.ejs', '<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8"><title></title></head><body><%- JSON.stringify(page) %></body></html>');
+      fs.writeFileSync('template/news/_detail.html.ejs', '<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8"><title></title></head><body><%=slug%> - <%=title%></body></html>');
+
+      fs.mkdirSync('template/live', { recursive: true });
+      fs.writeFileSync('template/live/_index.html.ejs', '<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8"><title></title></head><body><%- JSON.stringify(page) %></body></html>');
+      fs.writeFileSync('template/live/_detail.html.ejs', '<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8"><title></title></head><body><%=slug%> - <%=title%></body></html>');
     } catch (e) {
       throw e;
     }
@@ -116,17 +129,27 @@ describe('Run function', function() {
   });
   it('check file data.', function() {
     try {
-      stdout_src = execSync('find ./src -type f');
+      stdout_dist = execSync('find ./dist -type f');
       stdout_template = execSync('find ./template -type f');
-      stdout_read = fs.readFileSync('src/news/yamamoto/index.html', 'utf8');
-      stdout_read += '\n' + fs.readFileSync('src/live/adachi/index.html', 'utf8');
+
+      // index - news
+      stdout_read += '\n' + fs.readFileSync('dist/news/index.html', 'utf8');
+
+      // detail - news
+      stdout_read += '\n' + fs.readFileSync('dist/news/yamamoto/index.html', 'utf8');
+
+      // index -live
+      stdout_read += '\n' + fs.readFileSync('dist/live/index.html', 'utf8');
+
+      // detail - live
+      stdout_read += '\n' + fs.readFileSync('dist/live/adachi/index.html', 'utf8');
     } catch (e) {
       throw e;
     }
   });
   it('remove file data.', function() {
     try {
-      fs.removeSync('src');
+      fs.removeSync('dist');
       fs.removeSync('template');
     } catch (e) {
       throw e;
@@ -135,7 +158,7 @@ describe('Run function', function() {
   after(function(done) {
     console.log('---------------------');
     console.log('compile \n' + stdout_read + '\n');
-    console.log('src \n' + stdout_src);
+    console.log('dist \n' + stdout_dist);
     console.log('template \n' + stdout_template);
     done();
   });
